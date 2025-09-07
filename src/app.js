4f8da6b332916a413371392d42e34c18d40e00e5
Azul MA <azulma2826@gmail.com>
2025-09-07 22:38:44 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+// Función para generar carta aleatoria
+function getRandomCard() {
+  const palos = ['♥', '♠', '♣', '♦'];
+  const valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const palo = palos [Math.floor(Math.random() * palos.length)];
+  const valor = valores[Math.floor(Math.random() * valores.length)];
+    
+  document.getElementById('top').textContent = palo;
+  document.getElementById('valor').textContent = valor;
+  document.getElementById('bottom').textContent = palo;
+    
+  document.getElementById('card').style.color = (palo === '♥' || palo === '♦') ? 'red' : 'black';
+}
+
+window.onload =()=> {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  getRandomCard();
+  // Botón opcional
+  const newCardButton = document.getElementById("card-button")
+  newCardButton.addEventListener("click", getRandomCard)
+}
